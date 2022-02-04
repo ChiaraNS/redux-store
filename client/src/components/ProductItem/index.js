7c9d12534d740858dcff449d10cc0e9adc0c8832
Key <cns3.22202@gmail.com>
2022-02-03 19:55:44 -0600

    product items

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { pluralize } from "../../utils/helpers"
+import { pluralize } from "../../utils/helpers";
+import { ADD_MULTIPLE_TO_CART, ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
+import { useSelector, useDispatch } from 'react-redux';
+
 
 function ProductItem(item) {
+
+  const state = useSelector((state) => {
+    return state;
+  });
+
+  const dispatch = useDispatch();
+  const { cart } = state;
   const {
     image,
     name,
@@ -11,6 +22,24 @@ function ProductItem(item) {
     quantity
   } = item;
 
+  const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id)
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: _id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...item, purchaseQuantity: 1 }
+      });
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 })
+    }
+  }
+
+  
   return (
     <div className="card px-1 py-1">
       <Link to={`/products/${_id}`}>
